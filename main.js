b99a76d48665d44bdebaab3e1bcc63257ee7f1ee
caiquemoa <caiquemoa@gmail.com>
2023-01-04 10:33:02 -0300

    Refactored and fineshed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const board = document.getElementById('board')
 const gridSize = document.getElementById('gridSize')
-const gridLabel = document.getElementById('gridLabel')
+const rangeLabel = document.getElementById('rangeLabel')
 gridSize.addEventListener('change', createGrid)
 
 const body = document.getElementsByTagName('body')[0]
@@ -9,102 +9,107 @@ let mouseDown = false
 
 body.addEventListener('mouseup', () => (mouseDown = false))
 
-createGrid()
+const colorInput = document.getElementById('colorInput')
 
-const colorElement = document.getElementById('color')
-const resetGrid = document.getElementById('reset')
+colorInput.addEventListener('click', () => {
+	booleanReset()
+	color = colorInput.value
+})
+
+colorInput.addEventListener('change', () => {
+	booleanReset()
+	color = colorInput.value
+})
+
 const eraser = document.getElementById('eraser')
-const rainbow = document.getElementById('rainbow')
-const gridLines = document.getElementById('gridLines')
-const darken = document.getElementById('darken')
-const lighter = document.getElementById('lighter')
-
-let color = colorElement.value
-
-let randomColor = false
-
-let darkenColor = false
-
-let lightenColor = false
-
-colorElement.addEventListener('click', () => {
-	booleanReset()
-	color = colorElement.value
-})
-
-colorElement.addEventListener('change', () => {
-	booleanReset()
-	color = colorElement.value
-})
 
 eraser.addEventListener('click', () => {
 	booleanReset()
 	color = 'white'
 })
 
-rainbow.addEventListener('click', () => {
+const rgb = document.getElementById('rgb')
+
+let randomColor = false
+
+rgb.addEventListener('click', () => {
 	booleanReset()
 	randomColor = true
 })
 
+const resetGrid = document.getElementById('reset')
+
 resetGrid.addEventListener('click', createGrid)
 
-darken.addEventListener('click', () => {
-	booleanReset()
-	darkenColor = true
-})
-
-lighter.addEventListener('click', () => {
-	booleanReset()
-	lightenColor = true
-})
+const gridLines = document.getElementById('gridLines')
 
 gridLines.addEventListener('click', () => {
 	if (board.classList.contains('board')) return board.classList.remove('board')
 	board.classList.add('board')
 })
 
+const darken = document.getElementById('darken')
+let darkenColor = false
+
+darken.addEventListener('click', () => {
+	booleanReset()
+	darkenColor = true
+})
+
+const lighter = document.getElementById('lighter')
+
+let lightenColor = false
+
+lighter.addEventListener('click', () => {
+	booleanReset()
+	lightenColor = true
+})
+
 function createGrid() {
 	if (board.firstChild) cleanGird()
-	gridLabel.textContent = `${gridSize.value}x${gridSize.value}`
-	for (let i = 0; i < gridSize.value; i++) {
-		for (let j = 0; j < gridSize.value; j++) {
-			const item = document.createElement('div')
-			item.classList.add('gridItem')
+	rangeLabel.textContent = `${gridSize.value}x${gridSize.value}`
+	for (let i = 0; i < gridSize.value * gridSize.value; i++) {
+		const item = document.createElement('div')
+		item.style.backgroundColor = '#fff'
+		item.style.flex = `1 1 ${100 / gridSize.value}%`
+		item.style.userSelect = 'none'
 
-			item.addEventListener('mouseover', paintItem)
-			item.addEventListener('mousedown', () => {
-				mouseDown = true
-			})
-			item.addEventListener('mousedown', paintItem)
+		item.addEventListener('mouseover', paintItem)
 
-			board.appendChild(item)
-			item.style.flex = `1 1 ${100 / gridSize.value}%`
-			item.style.backgroundColor = '#fff'
-		}
+		item.addEventListener('mousedown', () => {
+			mouseDown = true
+		})
+
+		item.addEventListener('mousedown', paintItem)
+
+		board.appendChild(item)
 	}
 }
 
 function paintItem() {
-	if (mouseDown) {
-		if (randomColor) {
-			this.style.backgroundColor = `rgb(${Math.floor(
-				Math.random() * 245
-			)} ${Math.floor(Math.random() * 245)} ${Math.floor(Math.random() * 245)})`
-		} else if (darkenColor) {
-			this.style.backgroundColor = darkenLightenColor(
-				this.style.backgroundColor,
-				10
-			)
-		} else if (lightenColor) {
-			this.style.backgroundColor = darkenLightenColor(
-				this.style.backgroundColor,
-				-10
-			)
-		} else {
-			this.style.backgroundColor = color
-		}
+	if (!mouseDown) return
+	if (!randomColor & !darkenColor & !lightenColor)
+		return (this.style.backgroundColor = color)
+
+	if (randomColor) {
+		this.style.backgroundColor = getRandomColor()
+	} else if (darkenColor) {
+		this.style.backgroundColor = darkenLightenColor(
+			this.style.backgroundColor,
+			10
+		)
+	} else if (lightenColor) {
+		this.style.backgroundColor = darkenLightenColor(
+			this.style.backgroundColor,
+			-10
+		)
 	}
+}
+
+function getRandomColor() {
+	return `rgb(${Math.floor(Math.random() * 245)} ${Math.floor(
+		Math.random() * 245
+	)} ${Math.floor(Math.random() * 245)})`
 }
 
 function booleanReset() {
@@ -165,7 +170,8 @@ function darkenLightenColor(color, percent) {
 			')'
 		)
 	} else {
-		// A cor não é um formato válido
-		console.log('cor invalida')
+		console.log('invalid color')
 	}
 }
+
+createGrid()
